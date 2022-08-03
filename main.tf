adfe33a9cacc66b105a48a0c830401e3a786417a
vtechcloudgit <109309016+vtechcloudgit@users.noreply.github.com>
2022-08-03 11:29:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,84 +1,48 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.13.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
 
+
 provider "azurerm" {
-  
-}
-variable "prefix" {
-  default = "testterraform"
+  features {}
+
+  client_id = "25cdfab2-22f8-49c6-91e2-7b4a48196e87"
+  subscription_id = "86f09bbd-c75d-42f2-91e7-35b45b04e81d"
+  tenant_id = "1fbde232-2248-4587-aa11-d8655d6c9240"
+  client_secret = "8Je8Q~-fwGZ-dcbcwPRkNKhtYeU439w8ome_mbum"
+
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "West US "
+resource "azurerm_resource_group" "test" {
+  name     = "test-resources"
+  location = "West US"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+resource "azurerm_network_security_group" "test" {
+  name                = "test-security-group"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "example-network"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
+  
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
   }
-}
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "vtechadmin"
-    admin_password = "Cloud@123"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+  
   tags = {
-    environment = "staging"
+    environment = "Devlopment"
   }
 }
